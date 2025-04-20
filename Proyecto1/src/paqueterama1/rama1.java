8b970dd43f833c130505bc175381aac4daf0be34
fran9 <fran9@Fran>
2025-04-20 13:16:21 +0200

    Primer commit rama 1

---
+++ Proyecto1/src/paqueterama1/rama1.java
@@ -0,0 +1,8 @@
+package paqueterama1;
+
+public class rama1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Control de versiones rama 1");
+	}
+}
